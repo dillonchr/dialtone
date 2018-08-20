12d17276813465cb2cef85251c8c61d226dfe6fc
Dillon Christensen <dillon.christensen@kalibrate.com>
2018-08-20 14:38:04 -0500

    got html page for interactions

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-((context) => {
-    let keepRinging = true;
-    const audioCtx = new (context.AudioContext || context.webkitAudioContext)();
-    const gainNode = audioCtx.createGain();
-    gainNode.connect(audioCtx.destination);
-    gainNode.gain.value = 0.1;
-
-    context.stop = () => {
-        keepRinging = false;
-    };
+((context, gainCut) => {
+    const audio = new (context.AudioContext || context.webkitAudioContext)();
+    const gain = audio.createGain();
+    const tones = [];
+    gain.connect(audio.destination);
+    gain.gain.value = typeof gainCut !== 'undefined' ? gainCut : 0.1;
 
     const createOscillator = (ctx, freq) => {
           const o = ctx.createOscillator();
@@ -16,33 +12,52 @@
           return o;
     };
 
-    const dialBack = (gain) => {
-        const tones = [
-            createOscillator(audioCtx, 440),
-            createOscillator(audioCtx, 480)
-        ];
-        tones.forEach(o => {
-            o.connect(gain);
-            o.start(); 
-        });
-
-        setTimeout(() => {
-            tones.forEach(o => {
-                o.stop();
-                o.disconnect(gain);
-            });
-        }, 2000);
-
+    const dialBack = () => {
+        createDualTone(440, 480);
+        setTimeout(clearTones, 2000);
     };
 
-    const loop = () => {
-        if (!keepRinging) {
-            return console.log('STOPPED');
-        }
-        dialBack(gainNode);
-        setTimeout(loop, 6000);
-    }
+    const playTones = () => {
+        tones.forEach(o => {
+            o.connect(gain);
+            o.start();
+        });
+    };
 
-    loop();
+    const clearTones = () => {
+        tones.forEach(o => {
+            o.stop();
+            o.disconnect(gain);
+        });
+        tones.length = 0;
+    };
+
+    const createDualTone = (lof, hif) => {
+        tones.push(createOscillator(audio, lof));
+        tones.push(createOscillator(audio, hif));
+        playTones();
+    };
+
+    const bindNumber = (number, lof, hif) => {
+        const n = document.querySelector(`.number[data-number="${number}"]`);
+        n.addEventListener('mousedown', createDualTone.bind(context, lof, hif))
+        n.addEventListener('mouseup', clearTones);
+    };
+
+
+    [
+        [1, 697, 1209],
+        [2, 697, 1336],
+        [3, 697, 1477],
+        [4, 770, 1209],
+        [5, 770, 1336],
+        [6, 770, 1477],
+        [7, 852, 1209],
+        [8, 852, 1336],
+        [9, 852, 1477],
+        ['*', 941, 1209],
+        [0, 941, 1336],
+        ['#', 941, 1477]
+    ].forEach(([n, l, h]) => bindNumber(n, l, h));
 })(window);
 
